052639d028041da7360057182cb602ddaa23671e
andreffs18 <andreffs18@gmail.com>
2018-12-08 19:24:51 +0000

    Add 2018 day 1 in Elixir

---
+++ 2018/day1_puzzle1.exs
@@ -0,0 +1,35 @@
+defmodule Day1 do
+  @doc """
+  For example, if the device displays frequency changes of +1, -2, +3, +1, then starting from a frequency of zero, the following changes would occur:
+
+  Current frequency  0, change of +1; resulting frequency  1.
+  Current frequency  1, change of -2; resulting frequency -1.
+  Current frequency -1, change of +3; resulting frequency  2.
+  Current frequency  2, change of +1; resulting frequency  3.
+  In this example, the resulting frequency is 3.
+
+  Here are other example situations:
+
+  +1, +1, +1 results in  3
+  +1, +1, -2 results in  0
+  -1, -2, -3 results in -6
+  """
+
+  @spec main :: String.t()
+  def main do
+    IO.puts("### Day1 - Puzzle1 ###")
+
+    {params, _} = OptionParser.parse!(System.argv, strict: [filename: :string])
+
+    params[:filename]
+    |> File.read!
+    |> String.split
+    |> Enum.map(&String.to_integer(&1))
+    |> Enum.sum
+    |> IO.inspect
+  end
+
+end
+
+
+Day1.main()
---
+++ 2018/day1_puzzle2.exs
@@ -0,0 +1,49 @@
+defmodule Day1 do
+  @doc """
+  You notice that the device repeats the same frequency change list over and over. To calibrate the device, you need to find the first frequency it reaches twice.
+
+  For example, using the same list of changes above, the device would loop as follows:
+
+  Current frequency  0, change of +1; resulting frequency  1.
+  Current frequency  1, change of -2; resulting frequency -1.
+  Current frequency -1, change of +3; resulting frequency  2.
+  Current frequency  2, change of +1; resulting frequency  3.
+  (At this point, the device continues from the start of the list.)
+  Current frequency  3, change of +1; resulting frequency  4.
+  Current frequency  4, change of -2; resulting frequency  2, which has already been seen.
+  In this example, the first frequency reached twice is 2. Note that your device might need to repeat its list of frequency changes many times before a duplicate frequency is found, and that duplicates might be found while in the middle of processing the list.
+
+  Here are other examples:
+
+  +1, -1 first reaches 0 twice.
+  +3, +3, +4, -2, -4 first reaches 10 twice.
+  -6, +3, +8, +5, -6 first reaches 5 twice.
+  +7, +7, -2, -7, -4 first reaches 14 twice.
+  """
+
+  @spec main :: String.t()
+  def main do
+    IO.puts("### Day1 - Puzzle2 ###")
+
+    {params, _} = OptionParser.parse!(System.argv, strict: [filename: :string])
+
+    params[:filename]
+    |> File.read!
+    |> String.split
+    |> Enum.map(&String.to_integer(&1))
+    |> Stream.cycle
+    |> Enum.reduce_while([0], fn elem, acc ->
+      current_sum = elem + List.first(acc)
+      IO.inspect %{"elem" => elem, "acc" => acc, "current_sum" => current_sum}
+      case Enum.member?(acc, current_sum) do
+        true ->
+          {:halt, current_sum}
+        false ->
+          {:cont, [current_sum | acc]}
+      end
+    end)
+  end
+end
+
+
+Day1.main()
